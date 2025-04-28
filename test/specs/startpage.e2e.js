cd701812ac79eb9f2724f9e2639863e8a035e60f
Bob Bannister <bbannister@amplify.com>
2025-04-28 18:06:59 -0400

    broke landing page spec into two it blocks

--- test/specs/startpage.e2e.js
+++ test/specs/startpage.e2e.js
@@ -1,11 +1,14 @@
 import { expect } from '@wdio/globals'
 import StartPage from '../pageobjects/start.page.js'
 
-describe('Amplify.com', () => {
-    it('should go to the landing page', async () => {
+describe('Amplify.com: the user should go to ', () => {
+    it('the landing page and verify the logo is present', async () => {
         await StartPage.open()
 
         await expect($('.icon.icon--logo').toBeVisible())
+    })
+
+    it('the Our Programs section of the page, click on the CKLA square and check that the link is correct', async () => {
         await $('#op_amplify_ckla').click();
         //TO DO assert that we are on the expected page via new url
 
