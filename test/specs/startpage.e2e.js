7259a56172be260f19c16f0a3420b0c87b016241
Bob Bannister <bbannister@amplify.com>
2025-05-05 17:27:51 -0400

    for loop example in programs modal

--- test/specs/startpage.e2e.js
+++ test/specs/startpage.e2e.js
@@ -1,18 +1,21 @@
 import { expect } from '@wdio/globals'
 import StartPage from '../pageobjects/start.page.js'
 
+const programGroupArray = ['LITERACY SUITE', 'SCIENCE OF READING PROGRAMS', 'SPANISH LITERACY PROGRAMS', 'MIDDLE SCHOOL PROGRAMS','MATH & SCIENCE SUITE','MATH PROGRAMS', 'MATH PLATFORM', 'SCIENCE PROGRAM']
+
 describe('Amplify.com: the user should go to ', () => {
     it('the landing page and verify the logo is present', async () => {
         await StartPage.open()
         await expect($('.icon.icon--logo').toBeVisible())
     })
 
-    it('the Our Programs section of the page, click on the CKLA square and check that the link is correct', async () => {
-        await $('#op_amplify_ckla').click();
-        //Verifes if expected path/page is where code has taken us
-        await expect(StartPage.getUrlPath().includes('/programs/amplify-core-knowledge-language-arts/'));
+
+    it('the list of program headings in the programs modal, verifying the text', async () => {
+        await $('#m-programs').click();
+
+            for (let i = 0; i < 8; i++) {
+            const programGroups = await browser.$$('.m-dropdown__title')[i].getText();
+            await expect(programGroups).toContain(programGroupArray[i]);
+            }
     })
-
-
 })
-
