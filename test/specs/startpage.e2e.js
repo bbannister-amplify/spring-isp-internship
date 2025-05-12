e1ae9424a9c01348a6228570f4dc833c667aa7dc
Bob Bannister <bbannister@amplify.com>
2025-05-12 16:54:34 -0400

    moved accept cookies code to the page object file and called it from the test spec

--- test/specs/startpage.e2e.js
+++ test/specs/startpage.e2e.js
@@ -2,13 +2,13 @@ import { expect } from '@wdio/globals'
 import StartPage from '../pageobjects/start.page.js'
 
 describe('Amplify.com: the user should go to ', () => {
-    it('the landing page and verify the logo is present', async () => {
+    it('the landing page and verify the logo is present and accept cookies', async () => {
         await StartPage.open()
         await expect($('.icon.icon--logo').toBeVisible())
-        await expect($('.icon.icon--logo').toBeVisible())
-        if (await $('#augusta-ButtonElement--hKAdB82mq8CHpx8qWQKA').isDisplayed()) {
-           await $('#augusta-ButtonElement--hKAdB82mq8CHpx8qWQKA').click() }
+
+        await StartPage.acceptCookies();
     })
+
 
     it('the Our Programs section of the page, click on the CKLA square and check that the link is correct', async () => {
         await $('#op_amplify_ckla').click();
