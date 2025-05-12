b5f48bce7f786762513ee6618dfd88ea00711916
Bob Bannister <bbannister@amplify.com>
2025-05-12 15:27:30 -0400

    added accept cookies method

--- test/specs/startpage.e2e.js
+++ test/specs/startpage.e2e.js
@@ -5,6 +5,9 @@ describe('Amplify.com: the user should go to ', () => {
     it('the landing page and verify the logo is present', async () => {
         await StartPage.open()
         await expect($('.icon.icon--logo').toBeVisible())
+        await expect($('.icon.icon--logo').toBeVisible())
+        if (await $('#augusta-ButtonElement--hKAdB82mq8CHpx8qWQKA').isDisplayed()) {
+           await $('#augusta-ButtonElement--hKAdB82mq8CHpx8qWQKA').click() }
     })
 
     it('the Our Programs section of the page, click on the CKLA square and check that the link is correct', async () => {
